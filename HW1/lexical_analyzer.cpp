381e1d259bcc950717e4484c41f90afa600d4cd8
Diego <dcharrezt@unsa.edu.pe>
2018-09-08 17:12:02 -0500

    isID

--- HW1/lexical_analyzer.cpp
+++ HW1/lexical_analyzer.cpp
@@ -13,22 +13,55 @@ bool isDigit(char c);
 int main(int argc, char const *argv[]) {
 
 	char c;
+	string str;
 
-	cin >> c;
-	cout << isLetter(c) << endl;
-	cout << isDigit(c) << endl;
+	cin >> str;
+	cout << isIdentifier(str) << endl;
+	// cout << str.length() << endl;
+	// cout << str[3] << endl;
+
+
+	// cin >> c;
+	// cout << isLetter(c) << endl;
+	// cout << isDigit(c) << endl;
 	
 	return 0;
 }
 
 bool isIdentifier(string  str) {
-	// int state = 1;
-	// while(true) {
-	// 	switch(state):
-	// 	case 1:
-
-	// }
+	int state = 1;
+	int strLen = str.length();
+	int currentIndex = 0;
+	while(currentIndex < strLen) {
+		switch(state) {
+			case 1:
+				if( isLetter(str[currentIndex]) )
+					state = 3;
+				else  if( isDigit(str[currentIndex]) )
+					state = 2;
+				else
+					cout << "ERROR case 1, not digit nor letter"<< endl;	
+				currentIndex++; break;
+			case 2:
+				cout << "ERROR case 2, not digit nor letter" << endl;
+				currentIndex++; break;
+			case 3:
+				if( isLetter(str[currentIndex]) )
+					state = 3;
+				else if( isDigit )
+					state = 3;
+				else
+					cout << " ERROR case 3" << endl;
+				currentIndex++; break;
+		}
+	}
+	if(state != 3 ) {
+		cout << "Error" << endl; return false;
+	}
+	else
+		return true;
 }
+
 bool isNumber(string str);
 bool isReservedWord(string str);
 
